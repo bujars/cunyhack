bce671fc9ac0deaa304837ac112253803a079c00
EngerThen <engerthen4d17@gmail.com>
2019-10-19 14:53:49 -0400

    non profit profile screen

--- components/OrderItem.js
+++ components/OrderItem.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {CardItem} from 'native-base';
+import {Text, Image, StyleSheet} from 'react-native';
+import {CardItem, Icon, Right, Left, Button} from 'native-base';
 
 function OrderItem(props) {
+    const {picture, itemName, quantity, link} = props;
     return (
-        <CardItem>
-
-        </CardItem>
+        <Button style={styles.container}>
+            <Left style={styles.leftContainer}>
+                <Image source={{ uri: picture }} style={styles.image} />
+                <Text style={{color: 'grey'}}>  Item: </Text>
+                <Text>{itemName}</Text>
+                <Text style={{color: 'grey'}} >    quantity: </Text>
+                <Text>{quantity}</Text>
+            </Left>
+            <Right>
+                <Icon name="arrow-forward" />
+            </Right>
+        </Button>
     );
 }
 
-export default OrderItem;``
+const styles = StyleSheet.create({
+    container: {
+        borderBottomColor: '#eceded',
+        borderBottomWidth: 1,
+        justifyContent: 'space-between',
+        backgroundColor: '#fff',
+        padding: '5%'
+    },
+    leftContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    image: {
+        resizeMode: 'contain',
+        width: 30,
+        height: 50,
+        borderRadius: 50
+    }
+})
+
+export default OrderItem;
