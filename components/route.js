d2d1a086abe1e1d3dd0d91a9985aa74b05c67701
EngerThen <engerthen4d17@gmail.com>
2019-10-19 12:03:34 -0400

    naming issues with scenes

--- components/route.js
+++ components/route.js
@@ -3,19 +3,19 @@ import { Router, Scene } from 'react-native-router-flux'
 import { ScrollView } from 'react-native'
 import Landing from '../pages/landing'
 import MapPage from '../pages/map'
-import signUp from '../pages/signUp'
+import SignUp from '../pages/signUp'
 import NonProfitProfile from '../pages/NonProfitProfile';
 import Feed from '../pages/Feed'
 
 const Routes = (props) => (
     <Router cardStyle={{ backgroundColor: 'white' }}>
         <ScrollView>
-            <Scene key="signUp"  component={SignUp} initial={true}/>
+            <Scene key="signUp"  component={SignUp} />
             <Scene type="reset" component={Landing} title="Home" />
             <Scene key='feed' component={Feed} title='Feed' />
             <Scene type="reset" component={Landing} title="Home"  />
             <Scene key="map" component={MapPage} title="Map"/>
-            <Scene key="NonProfitProfile" component={NonProfitProfile} title="My Profile" />
+            <Scene key="NonProfitProfile" component={NonProfitProfile} title="My Profile" initial={true}/>
         </ScrollView>
     </Router>
 )
