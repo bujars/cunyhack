3b5498b6a63d767b6d0eb298c45e35accbf0c8d9
Jackie Lin <jackie@Jackies-MacBook-Pro.local>
2019-10-18 22:47:39 -0400

    updated

--- components/route.js
+++ components/route.js
@@ -3,13 +3,14 @@ import { Router, Scene } from 'react-native-router-flux'
 import { ScrollView } from 'react-native'
 import Landing from '../pages/landing'
 import MapPage from '../pages/map'
-import signUp from '../pages/signUp'
+import SignUp from '../pages/signUp'
+import Sign_Up from '../pages/sign_Up'
 
 const Routes = (props) => (
     <Router cardStyle={{ backgroundColor: 'white' }}>
         <ScrollView>
-            <Scene key="signUp"key="landing"  component={signUp} title="signUp" initial={true}/>
-            <Scene type="reset" component={Landing} title="Home"  />
+            <Scene key="signUp"  component={SignUp} initial={true}/>
+            <Scene type="reset" component={Landing} title="Home" />
             <Scene key="map" component={MapPage} title="Map"/>
         </ScrollView>
     </Router>
