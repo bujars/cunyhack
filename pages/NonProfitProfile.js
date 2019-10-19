9cfabe712ff7c8be385d37b957e1fbd9e9c0bbaf
EngerThen <engerthen4d17@gmail.com>
2019-10-18 21:31:41 -0400

    added redux's  actions folder

---
+++ pages/NonProfitProfile.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {StyleSheet, Image} from 'react-native';
+import {Container} from 'native-base';
+
+class NonProfitProfile extends Component {
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Left>
+                        <Image source={} />
+                    </Left>
+                </Header>
+            </Container>
+        )
+    }
+} 
