e7d0c4594f088964d14e29850c8c9fc2d110ca06
EngerThen <engerthen4d17@gmail.com>
2019-10-18 23:02:37 -0400

    improved non profie profile

---
+++ components/OrderItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function OrderItem(props) {
+    return <Text>{props.children}</Text>
+}
+
+export default OrderItem;
--- components/route.js
+++ components/route.js
@@ -4,6 +4,7 @@ import { ScrollView } from 'react-native'
 import Landing from '../pages/landing'
 import MapPage from '../pages/map'
 import signUp from '../pages/signUp'
+import NonProfitProfile from '../pages/NonProfitProfile';
 
 const Routes = (props) => (
     <Router cardStyle={{ backgroundColor: 'white' }}>
@@ -11,6 +12,7 @@ const Routes = (props) => (
             <Scene key="signUp"key="landing"  component={signUp} title="signUp" initial={true}/>
             <Scene type="reset" component={Landing} title="Home"  />
             <Scene key="map" component={MapPage} title="Map"/>
+            <Scene key="NonProfitProfile" component={NonProfitProfile} title="My Profile" />
         </ScrollView>
     </Router>
 )
