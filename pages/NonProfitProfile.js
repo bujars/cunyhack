e8745feff3eb12a423d97b8cedbc0a4d582b11d8
EngerThen <engerthen4d17@gmail.com>
2019-10-18 21:49:21 -0400

    changes to NonProfit Profile

--- pages/NonProfitProfile.js
+++ pages/NonProfitProfile.js
@@ -1,17 +1,41 @@
 import React, {Component} from 'react';
-import {StyleSheet, Image} from 'react-native';
-import {Container} from 'native-base';
+import {StyleSheet, Image, Text} from 'react-native';
+import {Container, Content, Left, CardItem, Body} from 'native-base';
+import { connect } from 'react-redux';
 
 class NonProfitProfile extends Component {
+
     render() {
+        const {profilePicture, name, address} = this.props.user;
         return (
             <Container>
                 <Header>
-                    <Left>
-                        <Image source={} />
-                    </Left>
+                    <Content>
+                        <Card>
+                            <Left>
+                                <Image  source={{uri: '#'}}/>
+                            </Left>
+                            <CardItem>
+                                <Text>{"Name"}</Text>
+                            </CardItem>
+                        </Card>
+                    </Content>
                 </Header>
+                <Body>
+                    <Content>
+                        <CardItem>
+                            <Text> This is a test </Text>
+                        </CardItem>
+                    </Content>
+                </Body>
             </Container>
         )
     }
-} 
+}
+
+function mapStateToProps(state) {
+    const { user } = state.users;
+    return { user }
+}
+
+export default connect(mapStateToProps)(NonProfitProfile);
