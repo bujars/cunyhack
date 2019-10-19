c08a01ccf55ff87f21be3d90bafe08511d4971a7
kartikeya01 <kartikeyasharma04@gmail.com>
2019-10-19 14:50:56 -0400

    changes

--- components/route.js
+++ components/route.js
@@ -3,22 +3,22 @@ import { Router, Scene } from 'react-native-router-flux'
 import { ScrollView } from 'react-native'
 import Landing from '../pages/landing'
 import MapPage from '../pages/map'
-<<<<<<< HEAD
 import SignUp from '../pages/signUp'
 import Feed from '../pages/Feed'
-=======
 import foodListingPage from '../pages/foodListing'
->>>>>>> 7d37e43... init
+import foodUploadPage from '../pages/foodUpload'
+
 
 const Routes = (props) => (
     <Router cardStyle={{ backgroundColor: 'white' }}>
         <ScrollView>
-            <Scene key="signUp"  component={SignUp} initial={true}/>
-            <Scene type="reset" component={Landing} title="Home" />
+            <Scene key="signUp"  component={SignUp} />
+            <Scene type="reset" component={Landing} title="Home" initial={true}/>
             <Scene key='feed' component={Feed} title='Feed' />
             <Scene type="reset" component={Landing} title="Home"  />
             <Scene key="map" component={MapPage} title="Map"/>
-            <Scene key="foodListing" component={foodListingPage} title="Upload leftover food"/>
+            <Scene key="foodListing" component={foodListingPage} title="Take a picture of your food"/>
+            <Scene key="foodUpload" component={foodUploadPage} title="Complete post"/>
         </ScrollView>
     </Router>
 )
