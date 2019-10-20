cd4bb598ca579f5424fda32c1a46ea30001841e9
kartikeya01 <kartikeyasharma04@gmail.com>
2019-10-19 22:20:31 -0400

    chnages

--- components/route.js
+++ components/route.js
@@ -10,7 +10,22 @@ import Feed from '../pages/Feed'
 import foodListingPage from '../pages/foodListing'
 import listingDetails from '../pages/listingDetails'
 import foodUploadPage from '../pages/foodUpload'
+import { Icon } from 'native-base';
+console.disableYellowBox = true;
 
+//Create a dedicated class that will manage the tabBar icon
+class TabIcon extends Component {
+    render() {
+      var color = this.props.selected ? '#00f240' : '#301c2a';
+  
+      return (
+        <View style={{flex:1, flexDirection:'column', alignItems:'center', alignSelf:'center', justifyContent: 'center'}}>
+          <Icon style={{color: color}} name={this.props.iconName || "circle"} size={18}/>
+          <Text style={{color: color, fontSize: 12}}>{this.props.title}</Text>
+        </View>
+      );
+    }
+}
 
 const Routes = (props) => (
     <Router cardStyle={{ backgroundColor: 'white' }}>
@@ -20,7 +35,7 @@ const Routes = (props) => (
             <Scene key='feed' component={Feed} title='Feed' />
             <Scene key='listingDetails' component={listingDetails} title='listingDetails' />
             <Scene key="landing" type="reset" component={Landing} title="Home" />
-            <Scene key="map" component={MapPage} initial={true} hideNavBar={1}/>
+
             <Scene key="SignIn" component={SignIn} title="signin"/>
             <Scene key="signUp"  component={SignUp}/>
             <Scene type="reset" component={Landing} title="Home" />
@@ -28,7 +43,22 @@ const Routes = (props) => (
             <Scene type="reset" component={Landing} title="Home"  />
             <Scene key="foodListing" component={foodListingPage} title="Take a picture of your food"/>
             <Scene key="foodUpload" component={foodUploadPage} title="Complete post"/>
-            <Scene key="NonProfitProfile" component={NonProfitProfile} title="My Profile" />
+
+            <Scene key='TabRoot' showLabel={false} hideNavBar tabs={true} initial={true}>
+                <Scene 
+                key="map" 
+                component={MapPage} 
+                initial={true} 
+                hideNavBar={1} 
+                initial
+                keyColor='black'
+                iconName='home'
+                iconColor='#3F7E44'
+                iconSize={24}
+                icon={TabIcon}
+                /> 
+                <Scene key="NonProfitProfile" component={NonProfitProfile} title="My Profile" />
+            </Scene>
         </ScrollView>
     </Router>
 )
